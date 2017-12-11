dc39b035d7ed0cf09594f9a772dd4b158df6f511
Peter Yakob <32325216+Peter-Yakob@users.noreply.github.com>
2017-12-11 18:31:07 +0100

    changes

--- Block 2 - Functional Programming/Code/src/Main.clj
+++ Block 2 - Functional Programming/Code/src/Main.clj
@@ -1,1 +1,21 @@
-(ns Main)
+(ns Main
+  (:import (java.io File FileReader)))
+
+(defmacro safe [Variable, Value], (eval ()))
+
+(comment
+  (def divide-zero (safe (/ 1 0)))
+  divide-zero
+
+  (def divide-two (safe (/ 10 2)))
+  divide-two
+
+  (def filefound (safe [s (FileReader. (File. "file.txt"))] (.read s)))
+  filefound
+
+  (def missing-file (safe [s (FileReader. (File. "missing-file"))] (. s read)))
+  missing-file
+  )
+
+(defn -main []
+  (println "Hello, World!"))
