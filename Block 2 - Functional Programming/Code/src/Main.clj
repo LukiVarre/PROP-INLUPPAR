d22cecc66cadeff71b98b53d93639b8068a3181b
Peter Yakob <32325216+Peter-Yakob@users.noreply.github.com>
2017-12-13 15:13:41 +0100

    Divide fungerar nu, måste fixa fil läsningen

--- Block 2 - Functional Programming/Code/src/Main.clj
+++ Block 2 - Functional Programming/Code/src/Main.clj
@@ -1,21 +1,29 @@
 (ns Main
-  (:import (java.io File FileReader)))
+  (:import (java.io File FileReader FileNotFoundException)))
 
-(defmacro safe [Variable, Value], (eval ()))
 
-(comment
-  (def divide-zero (safe (/ 1 0)))
-  divide-zero
+(defmacro safe
+  [code & vector]
+  (if (empty? vector)
+    `(try ~code (catch ArithmeticException e# (str "Divide by zero: " (.getMessage e#))))
 
-  (def divide-two (safe (/ 10 2)))
-  divide-two
+    `(try ~vector (catch FileNotFoundException e# (str "File not found: " (.getMessage e#))))))
 
-  (def filefound (safe [s (FileReader. (File. "file.txt"))] (.read s)))
-  filefound
 
-  (def missing-file (safe [s (FileReader. (File. "missing-file"))] (. s read)))
-  missing-file
-  )
+(def divide-zero (safe (/ 1 0)))
+divide-zero
+(println divide-zero)
 
-(defn -main []
-  (println "Hello, World!"))
+(def divide-two (safe (/ 10 2)))
+divide-two
+(println divide-two)
+
+(def filefound (safe [s (FileReader. (File. "file.txt"))] (.read s)))
+
+filefound
+
+(macroexpand '(safe [s (FileReader. (File. "file.txt"))] (.read s)))
+
+(def missing-file (safe [s (FileReader. (File. "missing-file"))] (. s read)))
+
+missing-file
