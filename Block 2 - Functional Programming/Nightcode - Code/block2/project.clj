e96523b238e09392f72f96a47c8d816e1e4b0c82
Peter Yakob <32325216+Peter-Yakob@users.noreply.github.com>
2017-12-14 22:44:54 +0100

    Jag hatar Clojure

---
+++ Block 2 - Functional Programming/Nightcode - Code/block2/project.clj
@@ -0,0 +1,5 @@
+(defproject block2 "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :aot [block2.core]
+  :main block2.core)
--- Block 2 - Functional Programming/Nightcode - Code/block2/src/block2/core.clj
+++ Block 2 - Functional Programming/Nightcode - Code/block2/src/block2/core.clj
@@ -1,45 +1,44 @@
-(ns Main
-  (:import (java.io File FileReader FileNotFoundException)))
-
+(ns block2.core
+  (:gen-class))
+(import '(java.io FileReader))
+(import '(java.io File))
+(import '(java.io FileNotFoundException))
 (comment
   (defmacro safe
     [vector & form]
     (if (empty? form)
-      `(try ~vector
-            (catch ArithmeticException e#
-              (str "Divide by zero: " (.getMessage e#))))
-
+      `(try ~vector 
+        (catch ArithmeticException e#
+         (str "Divide by zero: " (.getMessage e#))))
+       
       `(try ~form
-            (catch FileNotFoundException e#
-              (str "File not found: " (.getMessage e#))))))
-  )
+        (catch FileNotFoundException e#
+         (str "File not found: " (.getMessage e#)))))))
 
-
+  
 (defmacro safe
   [vector & form]
   (if (empty? form)
-    `(try ~vector
+    `(try ~vector 
       (catch ArithmeticException e#
        (str "Divide by zero: " (.getMessage e#))))
-
+    
      (let [f (get vector 0) l (get vector 1)]
        `(try ~form
          (catch FileNotFoundException e#
           (str "File not found: " (.getMessage e#)))))))
 
+(macroexpand '(safe [s (FileReader. (File. "file.txt"))] (.read s)))
+
 (def divide-zero (safe (/ 1 0)))
 divide-zero
-(println divide-zero)
 
 (def divide-two (safe (/ 10 2)))
 divide-two
-(println divide-two)
 
 (def filefound (safe [s (FileReader. (File. "file.txt"))] (.read s)))
 filefound
-(println filefound)
 
 (def missing-file (safe [s (FileReader. (File. "missing-file"))] (. s read)))
 missing-file
-(println missing-file)
-
+ 
