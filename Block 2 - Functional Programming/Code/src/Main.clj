9f7477fa6be79db57fe0220d4173c0bbc4d5cb97
Peter <peya2335@student.su.se>
2017-12-15 12:42:17 +0100

    Done

--- Block 2 - Functional Programming/Code/src/Main.clj
+++ Block 2 - Functional Programming/Code/src/Main.clj
@@ -1,28 +1,43 @@
 (ns Main
   (:import (java.io File FileReader FileNotFoundException)))
 
-(defmacro safe [vector & form]
-  (if (list? vector)
-    `(try
-       ~vector
-       (catch Throwable e#
-         (str "ArithmeticException java.lang.ArithmeticException: " (.getMessage e#))))
+(comment "Test Code"
 
-    (if (empty? vector)
+         (defmacro safe [vector & form]
+           (if (list? vector)
+             `(try ~vector)
+             (catch Throwable e#)
+             (str "ArithmeticException java.lang.ArithmeticException: " (.getMessage e#))
+
+             (if (empty? vector)
+               `(try ~form)
+               (catch Throwable e#)
+               (str "Error: Empty Vector" (.getMessage e#))
+
+               `(let ~(subvec vector 0 2))
+               (try ~@(subvec vector 2) ~@form)
+               (catch java.io.FileNotFoundException e#)
+               (str "FileNotFoundException java.io.FileNotFoundException:� " (.getMessage e#)))))
+
+         )
+
+
+(defmacro safe
+  [vector & form]
+  (if (empty? form)
+    `(try ~vector
+          (catch ArithmeticException e#
+            (str "ArithmeticException java.lang.ArithmeticException: " (.getMessage e#))))
+
+    (if (not= (count vector) 2)
       `(try ~form
             (catch Throwable e#
-              (str "Error: Empty Vector" (.getMessage e#))))
-
-      `(let ~(subvec vector 0 2)
-         (try ~@(subvec vector 2) ~@form
-              (catch Throwable e#
-                (str "FileNotFoundException java.io.FileNotFoundException: " (.getMessage e#))))))))
-
-
-
-(macroexpand '(safe [s (FileReader. (File. "C:/Users/Peter/Documents/GitHub/PROP-INLUPPAR/Block 2 - Functional Programming/file.txt"))] (.read s)))
-
-(macroexpand '(safe [s (FileReader. (File. "missing-file"))] (. s read)))
+              (str "Error: " (.getMessage e#))))
+      `(try
+         (let ~(subvec vector 0 2)
+           (try ~@(get vector 2) ~@form))
+         (catch java.io.FileNotFoundException e#
+           (str "FileNotFoundException java.io.FileNotFoundException: " (.getMessage e#)))))))
 
 (def divide-zero (safe (/ 1 0)))
 divide-zero
--- Block 2 - Functional Programming/Nightcode - Code/block2/src/block2/core.clj
+++ Block 2 - Functional Programming/Nightcode - Code/block2/src/block2/core.clj
@@ -4,48 +4,22 @@
 (import '(java.io File))
 (import '(java.io FileNotFoundException))
 
-(comment
-  (defmacro safe
-    [vector & form]
-    (if (empty? form)
-      `(try ~vector 
-        (catch ArithmeticException e#
-         (str "Divide by zero: " (.getMessage e#))))
-       
-      `(try ~form
-        (catch FileNotFoundException e#
-         (str "File not found: " (.getMessage e#)))))))
-
-(comment  
- (defmacro safe
-   [vector & form]
-   (if (empty? form)
-     `(try ~vector 
-       (catch ArithmeticException e#
-         (str "Divide by zero: " (.getMessage e#))))
-     
-     (let [f (get vector 0) l (get vector 1)]
-       `(try ~form
-         (catch FileNotFoundException e#
-          (str "File not found: " (.getMessage e#))))))))
-
-(defmacro safe [vector & form]
-  (if (list? vector)
-    `(try 
-       ~vector 
-      (catch Throwable e# 
-       (str "ArithmeticException java.lang.ArithmeticException: " (.getMessage e#))))
+(defmacro safe
+  [vector & form]
+  (if (empty? form)
+    `(try ~vector 
+      (catch ArithmeticException e#
+        (str "ArithmeticException java.lang.ArithmeticException: " (.getMessage e#))))
     
-    (if (empty? vector)
-      `(try ~form 
-        (catch Throwable e# 
-         (str "Error: Empty Vector" (.getMessage e#))))
-      
-      `(let ~(subvec vector 0 2)
-        (try ~@(subvec vector 2) ~@form
-         (catch Throwable e# 
-          (str "FileNotFoundException java.io.FileNotFoundException:� " (.getMessage e#))))))))
-
+    (if (not= (count vector) 2)
+       `(try ~form
+         (catch Throwable e#
+           (str "Error: " (.getMessage e#))))
+        `(try 
+          (let ~(subvec vector 0 2)
+           (try ~@(get vector 2) ~@form))
+          (catch java.io.FileNotFoundException e# 
+           (str "FileNotFoundException java.io.FileNotFoundException: " (.getMessage e#)))))))
 
 
 (macroexpand '(safe [s (FileReader. (File. "C:/Users/Peter/Documents/GitHub/PROP-INLUPPAR/Block 2 - Functional Programming/file.txt"))] (.read s)))
