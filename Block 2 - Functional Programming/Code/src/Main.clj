43245ffbb216eb67a665058769157adfeee31077
Peter <peteryakobte11@gmail.com>
2017-12-15 01:20:11 +0100

    Almost done!

--- Block 2 - Functional Programming/Code/src/Main.clj
+++ Block 2 - Functional Programming/Code/src/Main.clj
@@ -1,31 +1,28 @@
 (ns Main
   (:import (java.io File FileReader FileNotFoundException)))
 
-(comment
-  (defmacro safe
-    [vector & form]
-    (if (empty? form)
-      `(try ~vector
-            (catch ArithmeticException e#
-              (str "Divide by zero: " (.getMessage e#))))
+(defmacro safe [vector & form]
+  (if (list? vector)
+    `(try
+       ~vector
+       (catch Throwable e#
+         (str "ArithmeticException java.lang.ArithmeticException: " (.getMessage e#))))
 
+    (if (empty? vector)
       `(try ~form
-            (catch FileNotFoundException e#
-              (str "File not found: " (.getMessage e#))))))
-  )
+            (catch Throwable e#
+              (str "Error: Empty Vector" (.getMessage e#))))
+
+      `(let ~(subvec vector 0 2)
+         (try ~@(subvec vector 2) ~@form
+              (catch Throwable e#
+                (str "FileNotFoundException java.io.FileNotFoundException: " (.getMessage e#))))))))
 
 
-(defmacro safe
-  [vector & form]
-  (if (empty? form)
-    `(try ~vector
-      (catch ArithmeticException e#
-       (str "Divide by zero: " (.getMessage e#))))
 
-     (let [f (get vector 0) l (get vector 1)]
-       `(try ~form
-         (catch FileNotFoundException e#
-          (str "File not found: " (.getMessage e#)))))))
+(macroexpand '(safe [s (FileReader. (File. "C:/Users/Peter/Documents/GitHub/PROP-INLUPPAR/Block 2 - Functional Programming/file.txt"))] (.read s)))
+
+(macroexpand '(safe [s (FileReader. (File. "missing-file"))] (. s read)))
 
 (def divide-zero (safe (/ 1 0)))
 divide-zero
@@ -35,7 +32,7 @@ divide-zero
 divide-two
 (println divide-two)
 
-(def filefound (safe [s (FileReader. (File. "file.txt"))] (.read s)))
+(def filefound (safe [s (FileReader. (File. "C:/Users/Peter/Documents/GitHub/PROP-INLUPPAR/Block 2 - Functional Programming/file.txt"))] (.read s)))
 filefound
 (println filefound)
 
